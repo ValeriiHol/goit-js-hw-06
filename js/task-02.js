988a2d2e1cb237761d4b2037050dfc6e30e9550e
ValeriiHol <rielty@gmail.com>
2023-03-01 16:55:48 +0200

    add task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('#ingredients');
+const arrIngredients = [];
+
+ingredients.forEach(ingredient => {
+	const item = document.createElement('li');
+	item.className = 'item';
+	item.textContent = ingredient;
+	arrIngredients.push(item);
+
+})
+
+
+listIngredients.append(...arrIngredients);

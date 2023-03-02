e68937400d67229f7b1bdb0fba87f9163084d22c
ValeriiHol <rielty@gmail.com>
2023-03-02 12:00:54 +0200

    add task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const onChangeButton = document.querySelector('button.change-color');
+const bodyColor = document.querySelector('body');
+const colorValue = document.querySelector('span.color');
+
+onChangeButton.addEventListener('click', event => {
+  let color = getRandomHexColor();
+  bodyColor.style.backgroundColor = color;
+  colorValue.textContent = color;
+})

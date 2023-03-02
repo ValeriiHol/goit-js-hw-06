9f7957ea164898edfedff861085966cf1012e9f2
ValeriiHol <rielty@gmail.com>
2023-03-02 10:26:09 +0200

    add task6

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const textInput = document.querySelector('#validation-input');
+
+textInput.addEventListener('blur', (event) => {
+	if (event.target.value.length == textInput.getAttribute('data-length')) {
+		textInput.classList.add('valid');	
+		textInput.classList.remove('invalid');		
+	} else {
+        textInput.classList.add('invalid');
+		textInput.classList.remove('valid');
+	}
+});
+
